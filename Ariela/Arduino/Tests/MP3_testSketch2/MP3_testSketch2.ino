b8cf51795f61ad717f258124bd41c259c0593ab8
Ariela Mcclame <amcc949@aucklanduni.ac.nz>
2025-09-08 11:15:26 +1200

    added another test file

---
+++ Ariela/Arduino/Tests/MP3_testSketch2/MP3_testSketch2.ino
@@ -0,0 +1,61 @@
+
+#include <Arduino.h>
+#include <MD_YX5300.h>
+
+//HardwareSerial mp3Serial(2); // UART2
+//MD_YX5300 mp3(mp3Serial);
+
+const int rxPin = 16; // connect to TX of MP3 Player module
+const int txPin = 17; // connect to RX of MP3 Player module
+const int triggerPin = 34; // ADC1 pin for triggering
+const float thresholdVoltage = 2.8; // Hysteresis: 2.7V - 2.9V is when changes occur
+const float vRef = 3.3;
+const float maxADCValue = 4095;
+
+bool isPlaying = false; // track mp3Serial state
+
+void setup() {
+  Serial.begin(9600);
+  // Initialise player
+  //mp3Serial.begin(9600, SERIAL_8N1, rxPin, txPin);
+  analogReadResolution(12);
+  
+  //mp3.begin();
+  //mp3.setSynchronous(true); 
+  // Pause immediately on startup using raw command
+  //mp3.playStop();
+}
+
+void loop() {
+  //mp3.check();
+  // check the status of mp3; visit the library readme for more.
+  int adcValue = analogRead(triggerPin);
+  Serial.print("ADC value: ");
+  Serial.print(adcValue);
+  // find voltage by converting value from adc pin
+  float voltage = adcValue * vRef / maxADCValue;
+  Serial.print("Voltage: ");
+  Serial.print(voltage);
+  if (voltage >= (thresholdVoltage + 0.1)) {
+    // Run motor
+
+    if (!isPlaying) {
+      // play first song
+      isPlaying = true;
+      //mp3.playStart(); // maybe switch to mp3.playFolder(1, 1);
+      Serial.print("Playing song");
+    }
+  }
+  else if (voltage < (thresholdVoltage - 0.1)) {
+    // Stop motor
+    
+    if (isPlaying) {
+      // stop song
+      isPlaying = false;
+      //mp3.playStop();
+      Serial.print("Song stopped");
+    }
+  }
+  // delay so that not immediately retriggered
+  delay(100);
+}
